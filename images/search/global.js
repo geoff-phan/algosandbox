cc2419db54f460045f701fe42b69bcae1cfd25bd
geoff-phan <geoff.j.phan@outlook.com>
2021-06-22 03:24:22 -0400

    IndexerUpdate

--- images/search/global.js
+++ images/search/global.js
@@ -1,7 +1,7 @@
 // Global exports
 module.exports = Object.freeze({
     // dbhost: '127.0.0.1:5433', // Database URL
-    dbhost: 'postgresdb.server:5433', // Database URL
+    dbhost: 'http://127.0.0.1:5433', // Database URL
     dbuser: 'algorand', // Database user
     dbpass: 'algorand', // Database password
     // algodurl: 'http://127.0.0.1:4001', // Algod node endpoint
