7ff564de1c6d3a63db941e00f4365966a0b10626
geoff-phan <geoff.j.phan@outlook.com>
2021-06-24 09:07:22 -0400

    Update disabled.go

--- images/search/disabled.go
+++ images/search/disabled.go
@@ -13,7 +13,7 @@ func main() {
 	var code int
 	var message string
 
-	flag.IntVar(&port, "port", 8000, "port to start the server on.")
+	flag.IntVar(&port, "port", 8980, "port to start the server on.")
 	flag.IntVar(&code, "code", 400, "response code to use.")
 	flag.StringVar(&message, "message", "this is all that happens", "response message to display.")
 
